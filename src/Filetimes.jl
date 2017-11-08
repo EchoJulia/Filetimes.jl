edd90440a96964608be335bfc962fc4191525780
Rob Blackwell <rob@robblackwell.com>
2017-11-08 18:18:22 +0000

    Initial version

--- src/Filetimes.jl
+++ src/Filetimes.jl
@@ -1,5 +1,42 @@
 module Filetimes
 
-# package code goes here
+export filetime, datetime, EPOCH
+
+const EPOCH =  Dates.DateTime(1601, 1, 1)
+
+"""
+    filetime()
+
+Returns the current time as the number of 100 nanosecond intervals
+since January 1, 1601.
+
+"""
+function filetime()
+    filetime(now())
+end
+
+"""
+    filetime(d::Dates.DateTime)
+
+Returns the given DateTime as the number of 100 nanosecond intervals
+since January 1, 1601.
+
+"""
+function filetime(d::Dates.DateTime)
+    Dates.value(d - EPOCH) * 10000
+end
+
+"""
+    datetime(f::Integer)
+
+Returns a DateTime object given a filetime f, being the number of 100
+nanosecond intervals since January 1, 1601.
+
+N.B. Possible loss of precision.
+
+"""
+function datetime(f::Integer)
+    EPOCH + Dates.Microsecond(f/10)
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,12 @@
 using Filetimes
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+filetime()
+
+@test filetime(Dates.DateTime(1601, 1, 1)) == 0
+@test filetime(Dates.DateTime(1601, 1, 2)) == convert(UInt64, 24 * 60 * 60 * 1000 * 1000 * 10)
+
+@test Dates.year(Filetimes.datetime(131259265027478363)) == 2016
+
+x = now()
+@test Dates.year(Filetimes.datetime(Filetimes.filetime(x))) == Dates.year(x)
